499351ec5095de1b91e47ea70cbe1ea698c20629
Phu <phutrieu95@gmail.com>
2020-05-29 23:41:55 -0700

    Initialized firstNonRepeatingCharacter function

---
+++ firstNonRepeatingCharacter.js
@@ -0,0 +1,7 @@
+// given a string, write a function that takes the string and returns the first unique character
+// if no unique characters, return an underscore
+// e.g. 'aaabcccddde' returns 'b'
+
+const firstNonRepeatingCharacter = (string) => {
+
+}
