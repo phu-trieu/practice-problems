4d27aa589d4fe19a0e6330e55913267b2e3a8a6f
Phu <phutrieu95@gmail.com>
2020-06-04 01:19:15 -0700

    Initialized anagramsV2 function

---
+++ anagramsV2.js
@@ -0,0 +1,6 @@
+// create a function like the one in anagrams.js, but allow the function to take
+// any number of words, not just two
+// e.g. ['taco', 'coat', 'octa']
+const anagramsV2 = (array) => {
+  
+}
