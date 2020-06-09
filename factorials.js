b9cbd28edb5326b6cfefb8552e7465b17ca9ed3a
Phu <phutrieu95@gmail.com>
2020-06-08 23:04:01 -0700

    Initialized factorials function

---
+++ factorials.js
@@ -0,0 +1,8 @@
+// Do you know what a factorial of a number is? You can get the factorial of a
+// number by multiplying all the numbers less than or equal to that number. As
+// an example, 5! (5 factorial), is equal to 5 X 4 X 3 X 2 X 1 = 120. Would you
+// please write a solution for Robin that returns the factorial of that number?
+
+const factorial = (number) => {
+  
+}
