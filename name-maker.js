78f3ca66260f6d2aeb18d1550543ef20f72c7f08
Phu <phutrieu95@gmail.com>
2020-06-10 23:31:10 -0700

    Initialized name-maker function

---
+++ name-maker.js
@@ -0,0 +1,10 @@
+// You are a co-founder to a new, unnamed startup. After bickering for hours
+// over what the venture's name should be, the founding team makes a compromise.
+// They will take the first letter of each of their names, capitalized, and sorted
+// in alphabetical order - this will be the name of the startup. Because your
+// colleagues have pulled two all-nighters in a row, you decide to take one for the
+//  team and write a function that will return this new name.
+
+const nameMaker = (array) => {
+  
+}
