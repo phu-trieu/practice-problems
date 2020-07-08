2229e9bdea4cadcee231417adfd2f1be99f14807
Phu <phutrieu95@gmail.com>
2020-07-07 23:23:24 -0700

    Completed seven-boom function

--- seven-boom.js
+++ seven-boom.js
@@ -2,5 +2,7 @@
 // number 7 appears in the array.Otherwise, return "there is no 7 in the array".
 
 const sevenBoom = arr => {
-
+  return (arr.join('').includes(7) ? 'Boom!' : 'there is no 7 in the array');
 }
+
+sevenBoom([1,2,3,4,5]);
