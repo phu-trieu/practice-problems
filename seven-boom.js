3bf63d5fe0eaa9a57a2f45d39e38d4c3273a8653
Phu <phutrieu95@gmail.com>
2020-07-07 23:08:32 -0700

    Initialized seven boom function

---
+++ seven-boom.js
@@ -0,0 +1,6 @@
+// Create a function that takes an array of numbers and return "Boom!" if the
+// number 7 appears in the array.Otherwise, return "there is no 7 in the array".
+
+const sevenBoom = arr => {
+
+}
