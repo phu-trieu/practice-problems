9047bab1ae8690851fd7a615c407c63aad4da024
Phu <phutrieu95@gmail.com>
2020-07-19 21:47:07 -0700

    initialized balanced words function

---
+++ balanced-words.js
@@ -0,0 +1,10 @@
+// Have you heard about balanced words ? If a word is balanced, the sum of the
+// values on the left - hand side of the word is equal to the sum of the values on
+// the right - hand side.Each letter's value comes from its position in the alphabet
+// (a=1, b=2, etc.) and can be summed to determine the left and right hand sums. If
+// a word has an odd number of characters, the middle character should be ignored.
+// Can you write a function that determines whether or not a word is balanced?
+
+const balancedWords = string => {
+
+}
