aea133d72d0db22fbcdddb74f7c9f2de171a78f6
Phu <phutrieu95@gmail.com>
2020-07-23 23:19:38 -0700

    Initialized morse code function

---
+++ morse-code.js
@@ -0,0 +1,7 @@
+// You've been seeing strange series of flashing lights coming from a window in the
+// house across your street. One night, it dawns on you - it's morse code! Quick -
+// write a function that will decode the morse string and return the actual message.
+
+const morseCode = code => {
+
+}
