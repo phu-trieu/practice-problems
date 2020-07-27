43a7e4ce06331f40d5e231905729321028afda7b
Phu <phutrieu95@gmail.com>
2020-07-26 23:19:12 -0700

    Shortest steps array in progress

--- shortest-steps-array.js
+++ shortest-steps-array.js
@@ -2,3 +2,13 @@
 // with the shortest number of steps in between.In this case, you can assume that
 // the array of points is already sorted.For example, if S = { 1, 3, 4, 8, 13, 17, 20}
 // is given, the result will be(3, 4).
+
+const shortestStepsArr = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    let difference = Math.abs(arr[i] - arr[i-1]);
+    console.log(difference)
+
+  }
+}
+
+shortestStepsArr([1,3,4,8,12,17,20])
