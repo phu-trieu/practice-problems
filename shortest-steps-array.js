92f992f28e5cdeb4d0e00d1e0300f8479313fe7a
Phu <phutrieu95@gmail.com>
2020-07-26 23:06:10 -0700

    Initialized shortest steps array function

---
+++ shortest-steps-array.js
@@ -0,0 +1,4 @@
+// Write a function that, when given a list of integers, prints the pair of numbers
+// with the shortest number of steps in between.In this case, you can assume that
+// the array of points is already sorted.For example, if S = { 1, 3, 4, 8, 13, 17, 20}
+// is given, the result will be(3, 4).
