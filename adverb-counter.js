1f6c51e90f0a1c1204939ba74645e96301eafc1b
Phu <phutrieu95@gmail.com>
2020-07-28 18:34:59 -0700

    Initialized adverb counter function

---
+++ adverb-counter.js
@@ -0,0 +1,8 @@
+// An adverb is typically any word that ends in 'ly'.Can you write a function that
+// counts the number of words that ends in 'ly'.For this problem, even if the word
+// is an adjective, it's still okay to count it as true. However, you must not count
+// the word if it starts or contains 'ly' - it MUST be at the end. Happy coding!
+
+const adverbCounter = array => {
+
+}
