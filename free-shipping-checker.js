16dce1abeb566ff65bf5b17b43a779d264f102bb
Phu <phutrieu95@gmail.com>
2020-08-08 23:48:41 -0700

    initialized free shipping checker function

---
+++ free-shipping-checker.js
@@ -0,0 +1,13 @@
+// Been doing most of your shopping online ? It looks like your favorite store is
+// offering a promotion - if your total is greater than or equal to $30.00, then you
+// get free shipping! Please write a function that returns true if your order is
+// eligible for free shipping and false if it is not.
+
+//   freeShipping({ "Pens": 4.99, "Notebook": 3.99 }) ➞ false
+// freeShipping({ "Laptop": 999.99 }) ➞ true
+
+const freeShippingChecker = items => {
+
+}
+
+freeShippingChecker({"Pens": 4.99, "Notebook": 3.99});
