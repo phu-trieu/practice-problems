c2d5575eaa112431eadfeefac117b8cf0d92b6b3
Phu <phutrieu95@gmail.com>
2020-08-09 21:58:03 -0700

    Initialized even fibonacci sequence function

---
+++ even-fibonacci-sequence.js
@@ -0,0 +1,13 @@
+// Each term in the Fibonacci sequence is the sum of the two preceding terms.If you
+// start with 1 and 2, this sequence is:
+
+// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+// What is the total number of all even numbers in the Fibonacci sequence that are
+// less than 4, 000, 000 ?
+
+const evenFibonacciSequence = () => {
+
+}
+
+evenFibonacciSequence();
