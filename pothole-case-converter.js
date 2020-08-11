3ca4bb9b28c42beb0701c24b0b4a715df3782657
Phu <phutrieu95@gmail.com>
2020-08-10 22:35:02 -0700

    Initialized pothole case converter function

---
+++ pothole-case-converter.js
@@ -0,0 +1,11 @@
+// Some programming languages like Python prefers Pothole case over Camel case.
+
+// For instance: in JavaScript, you’d write yellowMonkey but in Python you’d write
+// yellow_monkey.In case you forget, you should just build a function where you
+// convert any string in Camel case to Pothole case.
+
+const potholeCaseConverter = str => {
+
+}
+
+potholeCaseConverter('fooBar');
