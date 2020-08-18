725bc755037d50e6524ced5f745fd6547e290061
Phu <phutrieu95@gmail.com>
2020-08-17 22:44:28 -0700

    Initialized duplicate value deleter function

---
+++ duplicate-value-deleter.js
@@ -0,0 +1,9 @@
+// Can you write a function that takes an array of values and remove all duplicate
+// elements in the array ? Make sure to return the array with only the unique values
+// remaining.
+
+const duplicateValueDeleter = arr => {
+
+}
+
+duplicateValueDeleter([1,1,2,2,3,3,3,3,3,4]);
