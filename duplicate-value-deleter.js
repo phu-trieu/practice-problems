595a3f52632437f3f44104f74d70a770a3ffab94
Phu <phutrieu95@gmail.com>
2020-08-17 23:33:59 -0700

    Completed duplicate value deleter function

--- duplicate-value-deleter.js
+++ duplicate-value-deleter.js
@@ -3,7 +3,7 @@
 // remaining.
 
 const duplicateValueDeleter = arr => {
-
+  return console.log(arr.filter((v, i) => arr.indexOf(v) === i))
 }
 
-duplicateValueDeleter([1,1,2,2,3,3,3,3,3,4]);
+duplicateValueDeleter([3, 1, 2, 2, 3, 3, 3, 1, 3, 4]);
