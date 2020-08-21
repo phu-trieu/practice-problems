08acc132a19645fa0a092b2e10022ffd8591431f
Phu <phutrieu95@gmail.com>
2020-08-21 14:40:58 -0700

    Initialized whos online function

---
+++ whos-online.js
@@ -0,0 +1,20 @@
+// Write a function that returns the number of users who are online in your slack
+// channel.Please include the following features:
+
+// 1) If nobody is online, return "There is nobody online."
+
+// 2) If one person is online, return "username online"
+
+// 3) If there are two people online, return "username and username2 are online
+
+// 4) If there are more than two people, return "username, username2, and # more
+// are online." So, if there were 6 people total online, you could return
+// "username, username2, and 4 more are online."
+
+// ex) whoIsOnline(["user123", "user4"]) ➞ "user123 and user4 are online"
+
+const whosOnline = arr => {
+
+}
+
+whoIsOnline(["user123", "user4"]);
