806a870bb4f540fb43c1e4e6fdca564ef5d16156
Phu <phutrieu95@gmail.com>
2020-08-25 15:08:42 -0700

    Initialized binary search function

---
+++ binary-search.js
@@ -0,0 +1,11 @@
+// Let's refresh one of the most basic and famous algorithm topics - Binary Search.
+// Here's a fundamental search challenge for you to tackle! Binary search algorithms
+// are often efficient, and they work by repeatedly dividing the list in half and
+// working with the portion that may contain the item being looked for until the
+// possible location is narrowed down to just one element.Try giving it a shot!
+
+const binarySearch = n => {
+
+}
+
+binarySearch(4);
