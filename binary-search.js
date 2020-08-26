5f2634d74cc29b6231c7fc99687f96d3f51c0e5a
Phu <phutrieu95@gmail.com>
2020-08-26 14:27:08 -0700

    Completed binary search function

--- binary-search.js
+++ binary-search.js
@@ -4,8 +4,20 @@
 // working with the portion that may contain the item being looked for until the
 // possible location is narrowed down to just one element.Try giving it a shot!
 
-const binarySearch = n => {
-
+const binarySearch = (n, arr, left, right) => {
+  const sorted = arr.sort((a, b) => a - b);
+  if (left === undefined) left = 0;
+  if (right === undefined) right = sorted.length - 1;
+  let mid = Math.floor(((right - left) / 2) + left);
+  if (right <= left && sorted[mid] !== n) {
+    return false;
+  } else if (sorted[mid] === n) {
+    return mid
+  } else if (sorted[mid] > n) {
+    return binarySearch(n, sorted, left, mid - 1)
+  } else if (sorted[mid] < n) {
+    return binarySearch(n, sorted, mid + 1, right)
+  }
 }
 
-binarySearch(4);
+console.log(binarySearch(1000, [1,3,45,27,18,99,69,42]));
