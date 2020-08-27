78b822c592943102a2d5fe958d8dd2292d92dcb1
Phu <phutrieu95@gmail.com>
2020-08-27 12:51:04 -0700

    Initialized spongebob case function

---
+++ spongebob-case.js
@@ -0,0 +1,8 @@
+// Write a function that accepts a string and returns the string with alternating
+// case.For example, "Hi there" would turn into "hI tHeRe".Make sure to ignore spaces!
+
+const spongebobCase = str => {
+
+}
+
+console.log(spongebobCase('Hello there'));
