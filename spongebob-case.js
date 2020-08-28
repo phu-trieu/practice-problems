292edf18083e353b7d9a8caec3bbb742e6ac339a
Phu <phutrieu95@gmail.com>
2020-08-27 17:04:14 -0700

    Completed spongebob case function

--- spongebob-case.js
+++ spongebob-case.js
@@ -2,7 +2,21 @@
 // case.For example, "Hi there" would turn into "hI tHeRe".Make sure to ignore spaces!
 
 const spongebobCase = str => {
+  const split = str.split(' ').map(x => x.split(''));
+  let a = true;
 
+  return split
+  .map(x => x.map(y => {
+    if (a) {
+      a = !a;
+      return y.toLowerCase();
+    } else {
+      a = !a;
+      return y.toUpperCase();
+    }
+  }))
+  .map(x => x.join(''))
+  .join(' ')
 }
 
-console.log(spongebobCase('Hello there'));
+console.log(spongebobCase('Good evening Mr Wayne'));
