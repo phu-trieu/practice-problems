a99a327dc9ce7e95ae743167ab359512cfea780e
Phu <phutrieu95@gmail.com>
2020-08-31 14:59:11 -0700

    Completed binary search iterative function

--- binary-search-iterative.js
+++ binary-search-iterative.js
@@ -1,5 +1,18 @@
-const binarySearch = arr => {
+const binarySearch = (num, arr) => {
+  let left = 0;
+  let right = arr.length - 1;
 
+  while (left <= right) {
+    const mid = Math.floor(((right - left) / 2) + left);
+    if (arr[mid] === num) {
+      return mid
+    } else if (arr[mid] > num) {
+      right = mid - 1;
+    } else if (arr[mid] < num) {
+      left = mid + 1;
+    }
+  }
+  return false;
 }
 
-binarySearch(24, [1, 6, 10, 15, 24, 26, 34, 36]);
+console.log(binarySearch(36, [1, 6, 10, 15, 24, 26, 34, 36]));
