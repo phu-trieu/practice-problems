ae6109647f85d8414f18437a3a42118201a8689b
Phu <phutrieu95@gmail.com>
2020-09-01 13:05:52 -0700

    Initialized remove last vowel function

---
+++ remove-last-vowel.js
@@ -0,0 +1,9 @@
+// Do you remember how to write a function that removes the last vowel from each
+// word in a sentence ? You must only remove the LAST instance of a vowel.For
+// example, "book" would turn into "bok".
+
+const removeLastVowel = str => {
+
+}
+
+console.log(removeLastVowel('Hey, VSauce Michael here'));
