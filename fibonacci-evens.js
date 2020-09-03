47144ba0683bf863e2624d1290b18699647474be
Phu <phutrieu95@gmail.com>
2020-09-02 23:20:46 -0700

    Completed fibonacci evens function

--- fibonacci-evens.js
+++ fibonacci-evens.js
@@ -3,8 +3,18 @@
 // sequence is: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...What is the total number of
 // all even numbers in the Fibonacci sequence that are less than 3,000,000?
 
-const fibonacciEvens = () => {
-
+const fibonacciEvens = num => {
+  const arr = [1,2];
+  let counter = 0;
+  while (arr[arr.length - 1] < num) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  }
+  arr.map(x => {
+    if (x % 2 === 0 && x < 3000000) {
+      counter++
+    }
+  })
+  return counter;
 }
 
-console.log(fibonacciEvens());
+console.log(fibonacciEvens(3000000));
