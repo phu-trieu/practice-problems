d674a0c10d3dbff4869a8c63320f2c5c7421731e
Phu <phutrieu95@gmail.com>
2020-09-02 22:51:19 -0700

    Initialized fibonacci evens function

---
+++ fibonacci-evens.js
@@ -0,0 +1,10 @@
+// Let's try working with the Fibonacci sequence again. Each term in the Fibonacci
+// sequence is the sum of the two preceding terms. If you start with 1 and 2, this
+// sequence is: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...What is the total number of
+// all even numbers in the Fibonacci sequence that are less than 3,000,000?
+
+const fibonacciEvens = () => {
+
+}
+
+console.log(fibonacciEvens());
