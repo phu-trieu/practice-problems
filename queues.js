f5c35ed8f70f54fc3a3cba2f8a119b37f26054e6
Phu <phutrieu95@gmail.com>
2020-09-03 23:08:26 -0700

    Initialized queues function

---
+++ queues.js
@@ -0,0 +1,4 @@
+// Let's have some review: it is up to you to create your own queue data structure.
+// Queues are FIFO - first in first out - in nature. Your queue should be a class
+// that has the methods "add" and "remove". Adding to the queue should store an
+// element until it is removed.
