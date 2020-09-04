b5bc5faa30452dc8d58000110acce7643c0fa603
Phu <phutrieu95@gmail.com>
2020-09-03 23:41:24 -0700

    Completed queues function

--- queues.js
+++ queues.js
@@ -2,3 +2,22 @@
 // Queues are FIFO - first in first out - in nature. Your queue should be a class
 // that has the methods "add" and "remove". Adding to the queue should store an
 // element until it is removed.
+
+class Queue {
+  constructor() {
+    this.arr = [];
+  }
+
+  add(value) {
+    this.arr.push(value)
+  }
+
+  remove() {
+    this.arr.shift();
+  }
+}
+
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.remove();
