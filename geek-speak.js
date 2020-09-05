e690090b1d2c0ba878fcaa9a65ad31c257a235c5
Phu <phutrieu95@gmail.com>
2020-09-04 23:16:54 -0700

    Initialized geek speak function

---
+++ geek-speak.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string and returns a coded version of the string.
+// Your function should replace all 'a's with '4's, 'e's with '3's, 'i's with '1's,
+// 'o's with '0's, and 's's with '5's.
+
+const geekSpeak = str => {
+
+}
+
+console.log(geekSpeak('Where\'s the lamb sauce'));
