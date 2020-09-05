32f6a41441d91cac2562702cee88452182dacf5b
Phu <phutrieu95@gmail.com>
2020-09-04 23:40:08 -0700

    Completed geek speak function

--- geek-speak.js
+++ geek-speak.js
@@ -3,7 +3,24 @@
 // 'o's with '0's, and 's's with '5's.
 
 const geekSpeak = str => {
-
+  return str.split('').map(v => {
+    if (v === 'a' || v === 'A') {
+      return v = '4'
+    }
+    if (v === 'e' || v === 'E') {
+      return v = '3'
+    }
+    if (v === 'i' || v === 'I') {
+      return v = '1'
+    }
+    if (v === 'o' || v === 'O') {
+      return v = '0'
+    }
+    if (v === 's' || v === 'S') {
+      return v = '5'
+    }
+    return v;
+  }).join('');
 }
 
 console.log(geekSpeak('Where\'s the lamb sauce'));
