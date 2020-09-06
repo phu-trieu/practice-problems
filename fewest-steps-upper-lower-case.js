1ae9b507da7b628954d5302c274dcf82e8a0c3d8
Phu <phutrieu95@gmail.com>
2020-09-05 23:45:07 -0700

    Initialized steps function

---
+++ fewest-steps-upper-lower-case.js
@@ -0,0 +1,11 @@
+// Write a function that returns the fewest number of steps it will take to convert
+// a string into uppercase or into lower case, whichever takes the least number of
+// steps.A step occurs when a character is changed from lower to upper case, or
+// vice versa.Return 0 if the string is empty or if there are no steps needed to be
+// taken.
+
+const steps = str => {
+
+}
+
+console.log(steps('I\'m gonna do what\'s called a pro gamer move'));
