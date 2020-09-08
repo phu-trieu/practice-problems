6a8def492bedd2ce9b5fc37663aae2fd1bf6e9d1
Phu <phutrieu95@gmail.com>
2020-09-07 22:31:00 -0700

    Initialized comma inserter function

---
+++ comma-inserter.js
@@ -0,0 +1,13 @@
+// Sometimes, you may be too lazy to add a comma between 0s to separate every
+// thousand.If you have your own converter that will just print out a number with
+// commas in right position you'd be happy! Today, write a function that converts
+// numeric strings to comma-separated strings. ※ However, do not use the money
+// conversion library supported by the programming language.
+
+// For example, 2000 -- > 2, 000 | -1224.10 -- > -1, 224.10
+
+const commaInserter = str => {
+
+}
+
+console.log(commaInserter('2000'));
