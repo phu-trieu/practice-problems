d87847650db3b778aef143e08a04d1d761c315e4
Phu <phutrieu95@gmail.com>
2020-09-11 22:53:23 -0700

    Initialized doubled pennies function

---
+++ doubled-pennies.js
@@ -0,0 +1,11 @@
+// You've been hired at a new company, and you earn one penny on your first day.
+// However, for each sequential day of work, your base amount doubles. This mean
+// that you will earn two pennies on your second day and four pennies on your third
+// day. Write a function that, give a number of days that you provide, will return
+// the total number of cumulative pennies that you will have.
+
+const doubledPennies = days => {
+
+}
+
+console.log(doubledPennies(5));
