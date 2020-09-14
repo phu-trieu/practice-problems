11a5844269e7a68fb0c232aee71c264ff040a52e
Phu <phutrieu95@gmail.com>
2020-09-13 22:24:44 -0700

    Initialized reverse string functg function

---
+++ reverse-string.js
@@ -0,0 +1,8 @@
+// Today's problem is short and to the point: Write a function that takes a string
+// and returns the string in reversed order.
+
+const reverseString = str => {
+
+}
+
+console.log(reverseString('olleH'));
