59dcc8e110294dd118c2831b212d3539cc40121b
Phu <phutrieu95@gmail.com>
2020-09-14 23:20:28 -0700

    Initialized fantastic fireworks function

---
+++ fantastic-fireworks.js
@@ -0,0 +1,12 @@
+// Create a function that accepts a number and returns a string variation of the
+// word "Boom".The string should include n number of "o"s, unless n is less than 2.
+// If that is the case, return "boom").If n is evenly divisible by 2, add an
+// exclamation point at the end of the string.If n is evenly divisible by 5, return
+// the string in all CAPS.If n is evenly divisible by both 2 and 5, return the
+// string in ALL CAPS and add an exclamation point to the end of the string.
+
+const fantasticFireworks = num => {
+
+}
+
+console.log(fantasticFireworks(10));
