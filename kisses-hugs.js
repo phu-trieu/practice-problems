3e584e36fe506d1029d707452679083318d70da6
Phu <phutrieu95@gmail.com>
2020-09-15 13:39:56 -0700

    Initialized kisses hugs function

---
+++ kisses-hugs.js
@@ -0,0 +1,9 @@
+// Write a function that receives a string and checks if that string has equal
+// numbers of 'x's and 'o's.If it does, return true.Otherwise, return false.If there
+// are no 'x's or 'o's, return true.
+
+const kissesHugs = str => {
+
+}
+
+console.log(kissesHugs('xoxo'));
