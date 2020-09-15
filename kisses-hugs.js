bd4be9ea3623fa98ff4c9d21525e214110712c34
Phu <phutrieu95@gmail.com>
2020-09-15 14:35:27 -0700

    Completed kisses hugs function

--- kisses-hugs.js
+++ kisses-hugs.js
@@ -3,7 +3,10 @@
 // are no 'x's or 'o's, return true.
 
 const kissesHugs = str => {
-
+  if (!str.includes('x') && !str.includes('o')) return true;
+  const x = str.split('').filter(letter => letter === 'x').length;
+  const o = str.split('').filter(letter => letter === 'o').length;
+  return (x === o ? true : false);
 }
 
-console.log(kissesHugs('xoxo'));
+console.log(kissesHugs('xoxo'), kissesHugs('xoxoo'), kissesHugs('hug, hug, kees, kees, hug, hug, big kees, little hug, kees, kees, little kees'));
