b2ecdee0b592f50248db5ab4c7f551afbcb6b6bb
Phu <phutrieu95@gmail.com>
2020-09-17 19:26:20 -0700

    Initialized first vowel function

---
+++ first-vowel.js
@@ -0,0 +1,9 @@
+// Write a function that takes in a string and returns the index of the first vowel
+// in the string.Y is not considered a vowel, the string should always contain a
+// vowel, and the string must be only one word.
+
+const firstVowel = str => {
+
+}
+
+console.log(firstVowel('henlo'));
