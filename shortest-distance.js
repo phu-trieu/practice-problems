5769086283a89a3767c5b7631b9e810d4f20c67d
Phu <phutrieu95@gmail.com>
2020-09-18 23:48:15 -0700

    Initialized shortest distance function

---
+++ shortest-distance.js
@@ -0,0 +1,12 @@
+// We'd love to share one of the interview questions asked during some bootcamp
+// students' first round interview at a Korean tech company.She was given one -
+// dimensional points array L = [1, 3, 4, 8, 13, 17, 20].The interviewer asked her
+// to find a pair of the shortest distances, given the one - dimensional points.
+// (Assume that the array of points is all sorted.).Can you ace the interview
+// question ?
+
+const shortestDistance = arr => {
+
+}
+
+console.log(shortestDistance([1,5,10,11,13,16]));
