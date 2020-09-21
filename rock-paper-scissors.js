b08b0b067465d70a14ec5a5279fb5b4f88cbec1a
Phu <phutrieu95@gmail.com>
2020-09-20 23:44:31 -0700

    Initialized rock paper scissors function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+// Do you remember that first simple project you had to build during your bootcamp
+//  - Rock, Paper, Scissors game ? You were perhaps not sure what you were supposed
+//  to do at that time.Since you've either learned lots of more complicated coding
+//  topics or built many applications, you should be able to quickly build a simple
+//  RPS game today. Robin challenges you!
+
+const rockPaperScissors = choice => {
+
+}
+
+console.log(rockPaperScissors('rock'));
