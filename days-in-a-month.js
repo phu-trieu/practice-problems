5c3cdfad9664ca7992f04261a944965b4751f569
Phu <phutrieu95@gmail.com>
2020-09-23 22:34:36 -0700

    Initialized days in a month function

---
+++ days-in-a-month.js
@@ -0,0 +1,8 @@
+// Write a function that accepts a month and a year as integers and returns the
+// number of days in that specific month.
+
+const daysInAMonth = date => {
+
+}
+
+console.log(daysInAMonth('March, 2020'));
