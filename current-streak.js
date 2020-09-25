efc6de35f4bdcd7133229374d511c8c651b3e969
Phu <phutrieu95@gmail.com>
2020-09-24 22:40:10 -0700

    Initialized current streak function

---
+++ current-streak.js
@@ -0,0 +1,11 @@
+// Have you been practicing your coding every day ? Now it's time for you to practice
+//  by writing a function that accepts the current date and a list of dates and
+//  returns how many days you have been on a streak for (in our case, how many
+//  consecutive days you have been practicing coding). If the array is empty,
+//  return 0.
+
+const currentStreak = arr => {
+
+}
+
+console.log(currentStreak([new Date(), new Date('September 23, 2020'), new Date('September 22, 2020')]))
