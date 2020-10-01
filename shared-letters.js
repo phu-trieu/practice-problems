d107684d4803f8a935e74cc7183e5569ab7178fb
Phu <phutrieu95@gmail.com>
2020-09-30 23:16:00 -0700

    Initialized shared letters function

---
+++ shared-letters.js
@@ -0,0 +1,9 @@
+// Write a function that receives two strings and returns a string that contains
+// only the letters that are shared between both strings.If there are none, return
+// an empty string.Put the string in alphabetical order before returning it.
+
+const sharedLetters = (str1, str2) => {
+
+}
+
+console.log(sharedLetters('bread', 'head'));
