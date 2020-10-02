75591d253ce5934586991d1ad7fee5f743b640d6
Phu <phutrieu95@gmail.com>
2020-10-01 23:20:53 -0700

    Initialized friday the 13th function

---
+++ friday-the-13th.js
@@ -0,0 +1,10 @@
+// Western superstition considers Friday the 13th to be an unlucky day.Write a
+// function that receives a month and year as numbers and returns whether that month
+// contains a Friday 13th or not(true or false).Months are not preceded with 0, so
+// January = 1, February = 2, etc.You can pass in the full year, like 2020 or 1975.
+
+const fridayThe13th = (month, year) => {
+
+}
+
+console.log(fridayThe13th(3, 2020));
