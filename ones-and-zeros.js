fd62ce068af5de7a8453588a9cb1a0bab533921f
Phu <phutrieu95@gmail.com>
2020-10-02 21:55:37 -0700

    Initialized ones and zeros function

---
+++ ones-and-zeros.js
@@ -0,0 +1,21 @@
+// I'm sure by now you know that computers operate in binary, right? Just a whole
+// bunch of 0s and 1s. These are very powerful numbers. A computer's processor is
+// made up of circuits that are composed of billions of transistors - switches that
+// are activated by the electronic signals it receives.The 1s and 0s reflect each
+// on and off state of a transistor.
+
+// Can you write a function that accepts a string of 0s and 1s and returns true if
+// every consecutive sequence of 1s is followed by a consecutive sequence of 0s of
+// the same length.
+
+// For example:
+
+// 111000110010 -- > true
+
+// 1001101110 -- > false
+
+const onesAndZeros = str => {
+
+}
+
+console.log(onesAndZeros('111000110010'));
