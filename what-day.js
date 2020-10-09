1e8a166379613020f19218c979cfde642a10f585
Phu <phutrieu95@gmail.com>
2020-10-09 00:22:35 -0700

    Added days of week as object

--- what-day.js
+++ what-day.js
@@ -5,7 +5,17 @@
 //   day it would be in 3 days, we would return ["Thursday", "Saturday", "Sunday"]
 
 const whatDay = (arr, n) => {
-  return new Date('Monday');
+  // return new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(new Date());
+  const days = {
+    Sunday: 0,
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6
+  }
+  return days[arr[0]];
 }
 
 console.log(whatDay(['Monday', 'Wednesday', 'Thursday'], 3));
