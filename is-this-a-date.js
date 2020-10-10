d97f205f7218b0ec8cbb5ba1adffdc3d683e3ef1
Phu <phutrieu95@gmail.com>
2020-10-10 16:26:56 -0700

    Initialized is this a date function

---
+++ is-this-a-date.js
@@ -0,0 +1,18 @@
+// What IS a date ? Can your code recognize a date ? Robin challenges you to write
+// a function that determines if an input is a date object or not.
+
+// You can try these input to test your code:
+
+// console.log(isDate("October 13, 2014 11:13:00"));
+
+// console.log(isDate(new Date(86400000)));
+
+// console.log(isDate(new Date(99, 5, 24, 11, 33, 30, 0)));
+
+// console.log(isDate([1, 2, 4, 0]));
+
+const isThisADate = value => {
+
+}
+
+console.log(isThisADate("October 13, 2014 11:13:00"));
