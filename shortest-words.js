3dfd81069d4111f098b10d94f2240b186b6f5c75
Phu <phutrieu95@gmail.com>
2020-10-13 20:45:06 -0700

    Initialized shortest words function

---
+++ shortest-words.js
@@ -0,0 +1,10 @@
+// Today's challenge lets us give respect to short words - words that are often
+// overlooked but are also often the most important glue in a sentence! Can you
+// write a function that, provided a string as an argument, returns the shortest
+// words in the string lowercase and in sorted in alphabetical order?
+
+const shortestWords = str => {
+
+}
+
+console.log(shortestWords('It\'s hot in Topeka'));
