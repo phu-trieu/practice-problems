03dcd9e35311b1f901930beaefdc6c89c6f6d1ce
Phu <phutrieu95@gmail.com>
2020-10-15 11:39:47 -0700

    Initialized date palindrome function

---
+++ date-palindrome.js
@@ -0,0 +1,11 @@
+// February 2nd, 2020 is a palindrome when viewed in mm / dd / yyyy and dd / mm / yyyy
+//  formats.Can you write a function that returns true if the date provided is a
+//  palindrome in both formats ?
+
+// 02 / 02/ 2020
+
+const datePalindrome = date => {
+
+}
+
+console.log(datePalindrome())
