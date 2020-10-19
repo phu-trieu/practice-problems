9b3a49ed12df04a3bcdc099a2c1910ea240a1b28
Phu <phutrieu95@gmail.com>
2020-10-18 23:42:46 -0700

    Initialized buckets of words function

---
+++ buckets-of-words.js
@@ -0,0 +1,14 @@
+// Its raining...words ? We need to collect words into our buckets! Let's try to
+// write a function that splits a sentence into buckets of words. Each bucket can
+// have a provided number of characters or less (spaces count as one character).
+// If the whole sentence is unable to fit inside buckets, then we have to return
+// an empty array :( Here is an example:
+
+// collectWords("the mouse can creep", 10)
+// ➞["the mouse", "can creep",]
+
+const collectWords = (str, num) => {
+
+}
+
+console.log(collectWords("the mouse can creep", 10));
