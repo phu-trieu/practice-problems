582aa501fa58b1f9a7958801688ac632bc2207e9
Phu <phutrieu95@gmail.com>
2020-10-26 23:10:54 -0700

    Initialized phone number formatter function

---
+++ phone-number-formatter-pt-2.js
@@ -0,0 +1,10 @@
+// When building web applications, it's often nice to format phone numbers that
+// users input on their own so that the user only has to input numbers. Write a
+// function that accepts ten numbers in an array and formats it like this:
+// "(123) 456-7890
+
+const phoneNumberFormatter = arr => {
+
+}
+
+console.log(phoneNumberFormatter([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
