6dbbf459e3d6d424cb93e46a58b935c390d491e1
Phu <phutrieu95@gmail.com>
2020-10-27 23:25:33 -0700

    Initialized moving day function

---
+++ moving-day.js
@@ -0,0 +1,11 @@
+// It's almost moving day! You have various boxes of different sizes and want to
+// know the total volume of all the boxes together. Why don't you write a function
+// that calculates the volume? The function will receive multiple arrays - each
+// array will represent a box. The arrays will have three elements each: length,
+// width, and height. Good luck!
+
+const movingDay = multiArr => {
+
+}
+
+console.log(movingDay([1, 2, 3], [4, 5, 6], [7, 8, 9]));
