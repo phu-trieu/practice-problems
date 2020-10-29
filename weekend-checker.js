997f85bc4a647dee584ebf99ddffe54b35c533bc
Phu <phutrieu95@gmail.com>
2020-10-28 18:29:01 -0700

    Initialized weekend checker function

---
+++ weekend-checker.js
@@ -0,0 +1,9 @@
+// Who doesn't love the weekend!? Can you write a function that, given a date in
+// the following format, returns whether that date is a weekend or not?
+// FORMAT: Oct 11, 2010
+
+const weekendChecker = date => {
+
+}
+
+console.log(weekendChecker('Oct 11, 2010'));
