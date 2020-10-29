799a7e23be389890574cf8a8c3454e8d94f2854d
Phu <phutrieu95@gmail.com>
2020-10-28 18:46:00 -0700

    Completed weekend checker function

--- weekend-checker.js
+++ weekend-checker.js
@@ -3,7 +3,7 @@
 // FORMAT: Oct 11, 2010
 
 const weekendChecker = date => {
-
+  return new Date(date).getDay() === 6 || new Date(date).getDay() === 0;
 }
 
 console.log(weekendChecker('Oct 11, 2010'));
