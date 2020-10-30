79ef91d8c2006916b1c0d78261b4b237f2fbce16
Phu <phutrieu95@gmail.com>
2020-10-29 22:53:55 -0700

    Initialized search pseudocode

---
+++ search-pseudocode.js
@@ -0,0 +1,13 @@
+// You are having a technical interview with a senior engineer today.Unfortunately,
+// you were too nervous last night and only slept for 2 hours.The interviewer asked
+// you to design a function to determine whether a company employee includes
+// Micheal Buble.Though you practiced many similar questions to the asked question,
+// you couldn't concentrate. Instead of designing the actual function, alternatively,
+// you decided to write in pseudocode. Write your pseudocode to at least surprise the
+// interviewer.
+
+const search = arr => {
+
+}
+
+console.log(search(['Kobe Bryant', 'Madonna', 'Snoop Dogg', 'Michael Buble']));
