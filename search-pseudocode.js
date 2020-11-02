5db1fcbc8dee1b12a979c1cd1e8ecd3629a74b16
Phu <phutrieu95@gmail.com>
2020-11-01 23:44:41 -0800

    Added more lines of pseudocode

--- search-pseudocode.js
+++ search-pseudocode.js
@@ -7,7 +7,15 @@
 // interviewer.
 
 const search = (arr, start, end) => {
-
+  // sort arr and assign to "sorted" variable
+  // if "start" not defined, set as 0
+  // if "end" not defined, set as length of "sorted" - 1
+  // define "mid" variable and set as average of "start" and "end"
+  // check if end is less than or equal to start and if "sorted" at "mid" index isn't equal to "Michael Buble"
+    // if true, return false
+  // check if "sorted" at "mid" index is equal to "Michael Buble"
+    // if true, return true
+  // check if value at "mid" index of "sorted" is less than
 }
 
 console.log(search(['Kobe Bryant', 'Madonna', 'Snoop Dogg', 'Michael Buble']));
