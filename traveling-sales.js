a02ada58192724254ac21e03f81e675558509bbd
Phu <phutrieu95@gmail.com>
2020-11-03 22:53:06 -0800

    Initialized traveling sales function

---
+++ traveling-sales.js
@@ -0,0 +1,13 @@
+// Surely you know about the famous Traveling Salesman problem ? If you have a list
+//  of cities and the distances between each pair of cities, what is the shortest
+//  possible route that visits each city and returns back to the starting point ?
+//  Write a function that receives "n" number of paths and returns the total number
+//  of possible paths that a salesman can travel.
+
+//   ex) paths(4)-- > 24
+
+const paths = n => {
+
+}
+
+console.log(paths(5));
