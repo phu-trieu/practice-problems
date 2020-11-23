4b07ba5a440067760db9d13b2850e82844a26f16
Phu <phutrieu95@gmail.com>
2020-11-22 23:28:44 -0800

    Completed which generation function

--- which-generation.js
+++ which-generation.js
@@ -25,7 +25,37 @@
 // 3	great grandson	great granddaughter
 
 const generation = (x, y) => {
-
+  const line = {
+    '-3': {
+      m: 'great grandfather',
+      f: 'great grandmother'
+    },
+    '-2': {
+      m: 'grandfather',
+      f: 'grandmother'
+    },
+    '-1': {
+      m: 'father',
+      f: 'mother'
+    },
+    '0': {
+      m: 'me!',
+      f: 'me!'
+    },
+    '1': {
+      m: 'son',
+      f: 'daughter'
+    },
+    '2': {
+      m: 'grandson',
+      f: 'granddaughter'
+    },
+    '3': {
+      m: 'great grandson',
+      f: 'great granddaughter'
+    }
+  }
+  return line[x + ''][y];
 }
 
-console.log(generation(2, 'f'));
+console.log(generation(-3, 'f'));
