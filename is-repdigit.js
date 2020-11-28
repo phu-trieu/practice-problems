b3e0f19ba5b312bf2e7c3d71947ac41502935d75
Phu <phutrieu95@gmail.com>
2020-11-27 22:54:03 -0800

    initialized is repdigit function

---
+++ is-repdigit.js
@@ -0,0 +1,17 @@
+// A repdigit is a positive number composed out of the same digit.Create a function
+// that takes an integer and returns whether it's a repdigit or not.
+
+// Examples
+// isRepdigit(66) ➞ true
+
+// isRepdigit(0) ➞ true
+
+// isRepdigit(-11) ➞ false
+// Notes
+// The number 0 should return true(even though it's not a positive number).
+
+const isRepdigit = num => {
+
+}
+
+console.log(isRepdigit(66));
