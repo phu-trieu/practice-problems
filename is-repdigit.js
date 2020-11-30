7e0ac55f428c172f6185c25ee0dadd3fa95996eb
Phu <phutrieu95@gmail.com>
2020-11-29 17:41:17 -0800

    Completed is repdigit function

--- is-repdigit.js
+++ is-repdigit.js
@@ -11,7 +11,7 @@
 // The number 0 should return true(even though it's not a positive number).
 
 const isRepdigit = num => {
-  const test = true;
+  return new Set('' + num).size === 1;
 }
 
 console.log(isRepdigit(66));
