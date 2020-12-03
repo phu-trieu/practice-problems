879d6b1db4987c08b442a53dc7fbb18575f69a04
Phu <phutrieu95@gmail.com>
2020-12-03 11:02:56 -0800

    Initialized basic arithmetic function

---
+++ basic-arithmetic.js
@@ -0,0 +1,24 @@
+// Write a function that does the following for the given values: add, subtract,
+// divide and multiply.This is simply referred to as the basic arithmetic operations.
+// The variables have to be defined, but in this challenge, it will be defined for you.
+// All you have to do, is to check the variables, do some string to integer conversion,
+// use some if conditions, and apply the arithmetic operation.
+
+// The numbers and operation are given as a string and should result to an integer value.
+
+//   Examples
+// operation("1", "2", "add") ➞ 3
+
+// operation("4", "5", "subtract") ➞ -1
+
+// operation("6", "3", "divide") ➞ 2
+// Notes
+// The numbers and operation are given as a string and should result to an integer value.
+// If the operation results to Infinity, then return "undefined"(e.g.division by zero).
+// Division results will be rounded down to its integral part.
+
+const operation = (a, b, op) => {
+
+}
+
+console.log(operation('1', '2', 'add'));
