40162fc86a6c7a84ab92e38c28bce4735e7474ae
Phu <phutrieu95@gmail.com>
2020-12-29 20:21:52 -0800

    Returned test data - str lowercased and split

--- replace-with-alphabet-position.js
+++ replace-with-alphabet-position.js
@@ -10,7 +10,7 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"(as a string)
 
 const alphabetPosition = str => {
-
+  return str.toLowerCase().split('');
 }
 
-console.log('The sunset sets at twelve o\'clock');
+console.log(alphabetPosition('The sunset sets at twelve o\'clock'));
