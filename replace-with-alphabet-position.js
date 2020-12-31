d1ce8b283ca57c6431f5f8c517542670397660ab
Phu <phutrieu95@gmail.com>
2020-12-30 23:58:00 -0800

    Returned each character's char code

--- replace-with-alphabet-position.js
+++ replace-with-alphabet-position.js
@@ -10,7 +10,9 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"(as a string)
 
 const alphabetPosition = str => {
-  return str.toLowerCase().split('');
+  let str2 = '';
+  str.toLowerCase().split('').forEach(char => str2 = ' ' + char.charCodeAt() + str2);
+  return str2.split(' ');
 }
 
 console.log(alphabetPosition('The sunset sets at twelve o\'clock'));
