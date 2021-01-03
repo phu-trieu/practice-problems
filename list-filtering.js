1fd91eec28ac890921e443ae35df2c44d909d7e3
Phu <phutrieu95@gmail.com>
2021-01-02 23:20:20 -0800

    Completed list filtering function

--- list-filtering.js
+++ list-filtering.js
@@ -6,8 +6,9 @@
 // filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
 // filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
 
-const filterList = arr => {
+const filterList = arr => arr.filter(char => typeof char === "number");
 
-}
 
 console.log(filterList([1, 2, 'a', 'b']));
+console.log(filterList([1, 'a', 'b', 0, 15]))
+console.log(filterList([1, 2, 'aasf', '1', '123', 123]))
