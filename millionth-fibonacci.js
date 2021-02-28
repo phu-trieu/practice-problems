4156539b4fb8bbdb864261df2c64a27204eba298
Phu Trieu <phutrieu95@gmail.com>
2021-02-27 20:35:21 -0800

    Initialized millionth fibonacci function

---
+++ millionth-fibonacci.js
@@ -0,0 +1,15 @@
+// In this kata you will have to calculate fib(n) where:
+
+// fib(0) := 0
+// fib(1) := 1
+// fin(n + 2) := fib(n + 1) + fib(n)
+// Write an algorithm that can handle n up to 2000000.
+
+// Your algorithm must output the exact integer answer, to full precision.Also,
+// it must correctly handle negative numbers as input.
+
+const fib = n => {
+
+}
+
+console.log(fib(25));
