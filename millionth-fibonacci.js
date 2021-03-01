886ee8d752bff8a2332ad7c8c5414c0355afa09c
Phu Trieu <phutrieu95@gmail.com>
2021-03-01 13:53:06 -0800

    Fibonacci function works up until n = 79, work in progree

--- millionth-fibonacci.js
+++ millionth-fibonacci.js
@@ -9,11 +9,46 @@
 // it must correctly handle negative numbers as input.
 
 const fib = n => {
-  const arr = [0, 1];
-  for (let i = 1; i < n; i++) {
-    arr.push(arr[i] + arr[i - 1]);
+  let arr = [0, 1];
+  if (n === 0) return BigInt(arr[0]);
+  if (Math.abs(n) === 1) return BigInt(arr[1]);
+  for (let i = 1; i < Math.abs(n); i++) {
+    if (n > 0) {
+      const sum = arr[0] + arr[1];
+      arr[0] = arr[1];
+      arr[1] = sum;
+    }
+    if (n < 0) {
+      const diff = arr[0] - arr[1];
+      arr[0] = arr[1];
+      arr[1] = diff;
+    }
   }
-  return arr;
+  // return (n > 0 ? BigInt(arr[arr.length - 1]) : BigInt((arr[arr.length - 1])))
+  return BigInt(arr[1]);
+
+  // return (((Math.pow((1 + Math.sqrt(5)), n) - Math.pow((1 - Math.sqrt(5)), n)) / (Math.pow(2, n) * Math.sqrt(5))));
 }
 
-console.log(fib(25));
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(-1));
+console.log(fib(-2));
+console.log(fib(-3));
+console.log(fib(-4));
+console.log(fib(-5));
+console.log(fib(-6));
+console.log(fib(-7));
+console.log(fib(-67));
+console.log(fib(-68));
+console.log(fib(-69));
+console.log(fib(78));
+// console.log(fib(-75));
+// console.log(fib(-76));
+// console.log(fib(-77));
